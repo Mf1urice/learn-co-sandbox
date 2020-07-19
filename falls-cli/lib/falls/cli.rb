1c42023181a9e9df11456ddd77ad347ef36f04ef
Maurice Falls <falls8019oct@gmail.com>
2020-07-19 14:05:11 +0000

    Automatically backed up by Learn

---
+++ falls-cli/lib/falls/cli.rb
@@ -0,0 +1,8 @@
+require "falls/cli/version"
+
+module Falls
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ falls-cli/lib/falls/cli/version.rb
@@ -0,0 +1,5 @@
+module Falls
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
